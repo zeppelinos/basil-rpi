7d061464a977baca6b1731e23b5cf5b9f2d135b6
ajs <palebluedot@gmail.com>
2018-05-16 18:22:30 -0300

    Use dynamic ip for Hue bridge

--- src/hue.js
+++ src/hue.js
@@ -2,12 +2,21 @@ const hue = require("node-hue-api");
 const converter = require("@q42philips/hue-color-converter");
 const HueApi = hue.HueApi;
 const lightState = hue.lightState;
-const { HUE_HOST, HUE_LED_ID, HUE_USERNAME } = require('./constants');
+const { HUE_LED_ID, HUE_USERNAME } = require('./constants');
 
 class Hue {
   constructor() {
     this.led = HUE_LED_ID;
-    this.api = new HueApi(HUE_HOST, HUE_USERNAME);
+    this.checkBridge()
+  }
+
+  checkBridge() {
+    const self = this
+    hue.nupnpSearch().then(function(bridges) {
+      console.log("Hue Bridges Found: " + JSON.stringify(bridges));
+      self.api = new HueApi(bridges[0].ipaddress, HUE_USERNAME)
+      self.setColor(0, 255, 0)
+    }).done();
   }
 
   setColor(red, green, blue) {
@@ -15,7 +24,6 @@ class Hue {
       const xy = converter.calculateXY(red, green, blue)
       const state = lightState.create()
         .xy(xy)
-      // console.log(`Hue: setting color ${JSON.stringify(state, null, 2)}`)
       this.api.setLightState(this.led, state, function(err, lights) {
         if(err) {
           console.log(`Hue: Error setting light: ${err}`)
