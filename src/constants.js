10c68b5f108375cd6c8f9173c287ece5eb137f44
ajs <palebluedot@gmail.com>
2018-05-24 16:54:34 -0300

    Use mainnet network

--- src/constants.js
+++ src/constants.js
@@ -10,12 +10,12 @@ module.exports = {
   HUE_USERNAME: process.env.HUE_USERNAME,
   
   // ROPSTEN
-  PROVIDER_URL: 'http://localhost:8555',
-  BASIL_ADDRESS: '0xd1ed7c1c27c0aa4169fff8f0a5bcd2675fad8781',
+  // PROVIDER_URL: 'http://localhost:8555',
+  // BASIL_ADDRESS: '0xd1ed7c1c27c0aa4169fff8f0a5bcd2675fad8781',
 
   // MAINNET
-  // PROVIDER_URL: 'http://localhost:8546',
-  // BASIL_ADDRESS: '0x96dc0af2a77e4b185e5fe6cbfdb21f281e17c733',
+  PROVIDER_URL: 'http://localhost:8546',
+  BASIL_ADDRESS: '0x96dc0af2a77e4b185e5fe6cbfdb21f281e17c733',
 
   // TWITTER
   TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY,
