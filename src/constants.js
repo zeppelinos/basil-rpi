6dd792e7cf5f4d7079d4f9aa8fcd88a8088593d5
ajs <palebluedot@gmail.com>
2018-05-24 16:49:47 -0300

    Use ropsten network

--- src/constants.js
+++ src/constants.js
@@ -8,10 +8,14 @@ module.exports = {
   HUE_HOST: process.env.HUE_HOST,
   HUE_LED_ID: process.env.HUE_LED_ID,
   HUE_USERNAME: process.env.HUE_USERNAME,
+  
+  // ROPSTEN
+  PROVIDER_URL: 'http://localhost:8555',
+  BASIL_ADDRESS: '0xd1ed7c1c27c0aa4169fff8f0a5bcd2675fad8781',
 
-  // ETHEREUM
-  PROVIDER_URL: 'http://localhost:8546',
-  BASIL_ADDRESS: '0x96dc0af2a77e4b185e5fe6cbfdb21f281e17c733',
+  // MAINNET
+  // PROVIDER_URL: 'http://localhost:8546',
+  // BASIL_ADDRESS: '0x96dc0af2a77e4b185e5fe6cbfdb21f281e17c733',
 
   // TWITTER
   TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY,
